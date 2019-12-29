526c303cc82068d78ed666029bf6961320592bd9
ka <djoubs54@gmail.com>
2019-12-29 22:24:24 +0000

    commit first1

--- app/src/main/java/com/example/multilayerperceptron/MainActivity.java
+++ app/src/main/java/com/example/multilayerperceptron/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("hhhhh", "onCreate: holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-        Log.i("hhhhhdsfgsr qz", "onCreate: holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        Log.i("hhhhhdsfgsr qz", "onCreate: holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
     }
 }
